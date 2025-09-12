978c03527088fff64f44d5d0701c021c0d3117a8
kyeongbok <kyeongbok_0627@kakao.com>
2025-09-12 22:45:14 +0900

    feat: admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,11 @@ import Layout from "./components/common/layouts/Layout";
 import ProductListTest from "./pages/ProductListTest";
 
 const queryClient = new QueryClient()
+import AdminDashboard from "./pages/Admin/AdminDashboard";
+import AdminLayout from "./components/common/layouts/admin/AdminLayout";
+import ProductAdminPage from "./pages/Admin/ProductAdminPage.jsx";
+import OrderAdminPage from "./pages/Admin/OrderAdminPage.jsx";
+
 
 export default function App() {
   return (
@@ -53,7 +58,15 @@ export default function App() {
                 />
               }
             />
-          </Routes>
+      
+      {/* 어드민 영역 */}
+      <Route path="/admin" element={<AdminLayout />}>
+        {/* /admin 진입 시 보여줄 페이지 */}
+        <Route index element={<AdminDashboard />} />
+        <Route path="/admin/products" element={<ProductAdminPage />} />
+        <Route path="/admin/orders" element={<OrderAdminPage />} />
+      </Route>
+    </Routes>
         </Suspense>
       </ErrorBoundary>
     </QueryClientProvider>
