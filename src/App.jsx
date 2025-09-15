4512d24e77d10aa14e18d551420122b4f8bb95ea
bottle-gwon <lazyguy1@daum.net>
2025-09-15 14:51:16 +0900

    [feat] 상품 리스트 뛰우기

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./pages/Home";
 import ProductDetailPage from "./pages/ProductDetailPage";
 
@@ -18,37 +19,41 @@ import { ErrorBoundary } from "./components/common/layouts/errors/ErrorBoundary"
 import "swiper/css";
 import Layout from "./components/common/layouts/Layout";
 
+const queryClient = new QueryClient()
+
 export default function App() {
   return (
-    <ErrorBoundary>
-      <Suspense fallback={<div className="p-6 text-sm text-gray-500">로딩 중…</div>}>
-        <Routes>
-          {/* 레이아웃이 필요한 페이지 */}
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary>
+        <Suspense fallback={<div className="p-6 text-sm text-gray-500">로딩 중…</div>}>
+          <Routes>
+            {/* 레이아웃이 필요한 페이지 */}
          <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/products/:id" element={<ProductDetailPage />} />
-         </Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/products/:id" element={<ProductDetailPage />} />
+           </Route>
 
-          {/* 상태코드별 에러 라우트 */}
-          <Route path="/errors/401" element={<Error401 />} />
-          <Route path="/errors/403" element={<Error403 />} />
-          <Route path="/errors/429" element={<Error429 />} />
-          <Route path="/errors/500" element={<Error500 />} />
-          <Route path="/errors/502" element={<Error502 />} />
-          <Route path="/errors/503" element={<Error503 />} />
-          <Route path="/errors/504" element={<Error504 />} />
-          <Route
-            path="*"
-            element={
-              <Error404
-                onSearch={(q) =>
-                  (window.location.href = `/search?q=${encodeURIComponent(q)}`)
-                }
-              />
-            }
-          />
-        </Routes>
-      </Suspense>
-    </ErrorBoundary>
+            {/* 상태코드별 에러 라우트 */}
+            <Route path="/errors/401" element={<Error401 />} />
+            <Route path="/errors/403" element={<Error403 />} />
+            <Route path="/errors/429" element={<Error429 />} />
+            <Route path="/errors/500" element={<Error500 />} />
+            <Route path="/errors/502" element={<Error502 />} />
+            <Route path="/errors/503" element={<Error503 />} />
+            <Route path="/errors/504" element={<Error504 />} />
+            <Route
+              path="*"
+              element={
+                <Error404
+                  onSearch={(q) =>
+                    (window.location.href = `/search?q=${encodeURIComponent(q)}`)
+                  }
+                />
+              }
+            />
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
+    </QueryClientProvider>
   );
 }
