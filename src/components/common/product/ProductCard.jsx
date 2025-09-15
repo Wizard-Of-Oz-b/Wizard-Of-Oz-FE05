b63804777b918808c3bb08ae5e9c4287b56c6596
bottle-gwon <lazyguy1@daum.net>
2025-09-15 14:24:22 +0900

    [feat] 프로덕트 리스트 페이지 추가 및 라우팅

--- src/components/common/product/ProductCard.jsx
+++ src/components/common/product/ProductCard.jsx
@@ -1,15 +1,27 @@
 import RatingStars from "./RatingStars";
 import { KRW } from "../models/product";
 
-export default function ProductCard({ product, onClick }) {
+// export default function ProductCard({ product, onClick }) {
+export default function ProductCard({data}) {
+
+
+  const onClickhandler = () => {
+    console.log('move to detail')
+  }
+
+  return(
+    <div className="flex flex-col cursor-pointer w-1/4  pb-8" onClick={onClickhandler}>
+      <img src={`https://picsum.photos/id/${data.product_id}/400/600`} alt={data.name}/>
+      <p className={`font-extralight ${data?.is_active? 'text-black': 'text-gray-400'}`}>{data.category_id}</p>
+      <p className={`font-medium ${data?.is_active? 'text-black': 'text-gray-400'}`}>{data.name}</p>
+      <p className={`font-bold ${data?.is_active? 'text-black': 'text-gray-400'}`}>{data.price.toLocaleString()}원</p>
+      <span>{data?.is_active ? '' : '품절'}</span>
+    </div>
+  )
 
 
 
 
-
-
-
-  
 
 
 
