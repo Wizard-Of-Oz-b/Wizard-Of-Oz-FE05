2a319706801b34c8d85e67361c7ff0565bb6d69c
bottle-gwon <lazyguy1@daum.net>
2025-09-15 14:28:27 +0900

    [test] 테스트 목데이터 추가

--- src/components/common/product/ProductList.jsx
+++ src/components/common/product/ProductList.jsx
@@ -16,19 +16,20 @@ export default function ProductList({datas, isLoading, query, onSortChange, onPa
   <div className="w-dvw flex items-center justify-center">
 
     <div className="w-3/4">
-        <div className="flex items-center justify-between">
-          <p className="font-medium text-lg">{`결과: ${datas.total}개`}</p>
-          <ProductDropDown/>
-          {/* <p className="font-medium">정렬 기준</p> */}
-        </div>
-        <div className="flex flex-wrap items-start  pt-5">
-          {datas?.items.map((el)=> <ProductCard key={el.product_id} data={el}/>)}
-        </div>
-        <div className="flex items-center justify-center">
-          <button>이전</button>
-          <span>{datas.page}/{totalPage}</span>
-          <button>다음</button>
-        </div>
+			<div className="flex items-center justify-between">
+				<p className="font-medium text-lg">{`결과: ${datas.total}개`}</p>
+				<ProductDropDown/>
+				{/* <p className="font-medium">정렬 기준</p> */}
+			</div>
+			
+			<div className="flex flex-wrap items-start  pt-5">
+				{datas?.items.map((el)=> <ProductCard key={el.product_id} data={el}/>)}
+			</div>
+			<div className="flex items-center justify-center">
+				<button>이전</button>
+				<span>{datas.page}/{totalPage}</span>
+				<button>다음</button>
+			</div>
     </div>
 
   </div>
