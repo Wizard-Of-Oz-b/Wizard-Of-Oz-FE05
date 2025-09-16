35ae81c89314512035d31c9065be20b58b142883
bottle-gwon <lazyguy1@daum.net>
2025-09-16 13:14:32 +0900

    [feat] getProudcts 함수 목데이터와 분리

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -2,7 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { createQueryString } from "../utils/createQueryString";
 
-async function getProducts() {
+const BASE_URL = '/api/v1/products'
+
+async function getMockProducts() {
   try {
     const response = await axios.get('/api/v1/products');
     console.log(response.data)
@@ -13,14 +15,30 @@ async function getProducts() {
   }
 }
 
+async function getProducts(params) {
+  try {
+    const response = await axios.get(BASE_URL+'?'+params);
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.error(`상품 가져오기 에러${error}`);
+    if(error instanceof Error){
+      throw error
+    }else{
+      throw new Error(String(error));
+    }
+  }
+}
+
+
 export function useProducts(params) {
   
   const query = createQueryString(params)
-  
+
   console.log(query, '예시');
   return useQuery({
     queryKey: ['products', params],
-    queryFn: getProducts,
+    queryFn: getMockProducts,
     staleTime: 1000 * 60 * 5, //5분 지속
 
   })
