04733c022c281482ad78dd220bcc9f070a495e1b
bottle-gwon <lazyguy1@daum.net>
2025-09-16 10:11:38 +0900

    [feat] 상품 목록 페이지네이션 추가

--- src/components/common/product/ProductPagination.jsx
+++ src/components/common/product/ProductPagination.jsx
@@ -1,11 +1,40 @@
-export default function ProductPagination({currentPage, totalPage, onPageChange}) {
+export default function ProductPagination({currentPage, totalPage , onPageChange}) {
 
+	const pageNumbers = Array.from({length: totalPage}, (_,i) => i+1);
+	console.log(pageNumbers.length, '길이')
+
+	pageNumbers.map((el,index) =>(console.log(index,'인덱스')))
 	return(
-		<div className="flex items-center justify-center">
-			<button>이전</button>
-			<span>{currentPage}/{totalPage}</span>
-			<button>다음</button>
-		</div>
+		    <div className="flex justify-center space-x-2"> 
+      {/* '이전' 버튼 */}
+      <button
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+        className="px-4 py-2 border rounded-md disabled:opacity-50"
+      >
+        이전
+      </button>
 
+      {/* 페이지 번호 버튼들 */}
+      {pageNumbers.map((number) => (
+        <button
+          key={number}
+          onClick={() => onPageChange(number)}
+          className={`px-4 py-2 border rounded-md 
+            ${currentPage === number ? 'bg-violet-600 text-white' : 'bg-white'}`} // 현재 페이지 강조 스타일
+        >
+          {number}
+        </button>
+      ))}
+
+      {/* '다음' 버튼 */}
+      <button
+        onClick={() => onPageChange(currentPage + 1)}
+        disabled={currentPage === totalPage}
+        className="px-4 py-2 border rounded-md disabled:opacity-50"
+      >
+        다음
+      </button>
+    </div>
 	)
 }
