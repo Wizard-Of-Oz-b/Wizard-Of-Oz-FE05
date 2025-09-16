537d8492b2e4f98ab052f9bf7250811502b34e72
wnsgh981013 <wnsgh19981013@gmail.com>
2025-09-16 13:36:15 +0900

    fix:레이아웃수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import "swiper/css";
-import Mypage from './pages/Mypage';
 import Layout from "./components/common/layouts/Layout";
 
 export default function App() {
@@ -12,7 +11,6 @@ export default function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/products/:id" element={<ProductDetailPage />} />
-        <Route path="/mypage" element={<Mypage />} />
       </Route>
     </Routes>
   );
