1a9d1e96d0a1818464d8519f8d98adfcb3449b60
bottle-gwon <lazyguy1@daum.net>
2025-10-01 13:15:01 +0900

    fix: 장바구니 상품 없으면 주문,삭제 버튼 미출력 구현

--- src/pages/UserCart.jsx
+++ src/pages/UserCart.jsx
@@ -65,7 +65,7 @@ export default function UserCart() {
           <tr>
             <td colSpan={5}>
               <div className="flex w-full justify-end mt-2">
-                {cart ? (
+                {cart?.item_count >=1 ? (
                   <button
                     onClick={handleClearCart}
                     className="border border-gray-300 px-5 py-1 cursor-pointer"
@@ -95,7 +95,7 @@ export default function UserCart() {
         >
           쇼핑 계속하기
         </button>
-        {cart ? (
+        {cart?.item_count >=1 ? (
           <button
             className="border border-gray-300 text-xl px-8 py-2 mx-2 bg-black text-white cursor-pointer"
             onClick={handlePurchaseClick}
@@ -109,63 +109,4 @@ export default function UserCart() {
       </div>
     </div>
   );
-
-  // return (
-  //   <div className="flex w-full  justify-center">
-  //     <div className="flex flex-col w-full items-center justify-center border-x-gray-600 pb-20">
-  //       <p className="text-4xl mb-3">장바구니</p>
-  //       <div>
-  //         <CartToolbar />
-  //       </div>
-  //       <div className="flex flex-col mt-3">
-  //         <div className="flex flex-col mr-4">
-  //           {/* 나중에 상품 없음 컴포넌트 추가 할것 중요! */}
-  //           {/* {cartList.length === 0 ? "상품없음" : null}
-  //           {cartList.map((el) => (
-  //             <CartCard
-  //               key={el.id}
-  //               data={el}
-  //             />
-  //           ))} */}
-  //           {cart?.items.lengnth === 0 ? "상품없음" : null}
-  //           {cartList.map((el) => (
-  //             <CartCard key={el.id} data={el} />
-  //           ))}
-  //         </div>
-
-  //         <div className="flex w-full justify-end mt-2">
-  //           {/* <button className="border border-gray-300 px-5 py-1">선택상품 삭제</button> */}
-  //           <button
-  //             onClick={handleClearCart}
-  //             className="border border-gray-300 px-5 py-1"
-  //           >
-  //             장바구니 비우기
-  //           </button>
-  //         </div>
-  //         <span className="text-sm mt-1">
-  //           ※ {(50000).toLocaleString()}원 이상 구매시 배송비 무료{" "}
-  //         </span>
-  //         <OrderSummary sumPrice={cart?.items_total} />
-  //         <CartDays />
-  //       </div>
-  //       <div className="flex items-center justify-center mt-3">
-  //         {/* <button className="border border-gray-300 text-xl px-8 py-2 mx-2">선택 상품 주문</button> */}
-  //         <button
-  //           className="border border-gray-300 text-xl px-8 py-2 mx-2"
-  //           onClick={OnClickShopping}
-  //         >
-  //           쇼핑 계속하기
-  //         </button>
-
-  //         <button
-  //           className="border border-gray-300 text-xl px-8 py-2 mx-2 bg-black text-white"
-  //           onClick={handlePurchaseClick}
-  //           disabled={purchaseMutation.isPending}
-  //         >
-  //           전체 상품 주문
-  //         </button>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 }
